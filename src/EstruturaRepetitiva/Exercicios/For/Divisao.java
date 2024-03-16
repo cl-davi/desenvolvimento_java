b701867945ddabe73dd039f56ddd839b42268c94
Davi <davifsclmt@gmail.com>
2024-03-16 20:50:29 -0300

    Exercício para fazer a operação de divisão

---
+++ src/EstruturaRepetitiva/Exercicios/For/Divisao.java
@@ -0,0 +1,30 @@
+package EstruturaRepetitiva.Exercicios.For;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Divisao {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe a quantidade de operações: ");
+        int n = scanner.nextInt();
+
+        double divisao;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Informe o numerador: ");
+            double numerador = scanner.nextDouble();
+            System.out.print("Informe o denominador: ");
+            double denominador = scanner.nextDouble();
+
+            if (denominador == 0) {
+                System.out.println("Divisão impossível");
+            } else {
+                divisao = numerador / denominador;
+                System.out.println(divisao);
+            }
+        }
+    }
+}
