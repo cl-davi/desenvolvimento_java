8d007f35a7fffca0c267f17ca36a9252aa7d842c
Davi <davifsclmt@gmail.com>
2024-03-16 20:53:45 -0300

    Exercício para efetuar uma exponenciação

---
+++ src/EstruturaRepetitiva/Exercicios/For/ExponenciacaoAlinhada.java
@@ -0,0 +1,19 @@
+package EstruturaRepetitiva.Exercicios.For;
+
+import java.util.Scanner;
+
+public class ExponenciacaoAlinhada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe o valor: ");
+        int valor = scanner.nextInt();
+
+        for (int i = 1; i <= valor; i++) {
+            double segundo = Math.pow(i, 2);
+            double terceiro = Math.pow(i, 3);
+            System.out.println((int)i + " " + (int)segundo + " " + (int)terceiro);
+        }
+        scanner.close();
+    }
+}
