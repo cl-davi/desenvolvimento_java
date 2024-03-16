02ab76718705c5a6fce344635e29d9be88de9b5f
Davi <davifsclmt@gmail.com>
2024-03-16 15:16:31 -0300

    Exercício para calcular a duração de uma partida de um jogo

---
+++ src/EstruturaCondicional/Exercicios/DuracaoJogo.java
@@ -0,0 +1,24 @@
+package EstruturaCondicional.Exercicios;
+
+import java.util.Scanner;
+
+public class DuracaoJogo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Hora inicial: ");
+        int horaInicial = scanner.nextInt();
+        System.out.print("Hora final: ");
+        int horaFinal = scanner.nextInt();
+
+        int duracaoJogo;
+
+        if (horaInicial < horaFinal) {
+            duracaoJogo = horaFinal - horaInicial;
+        } else {
+            duracaoJogo =  24 - horaInicial + horaFinal;
+        }
+        System.out.println("O JOGO DUROU: " + duracaoJogo + " HORA(S)");
+        scanner.close();
+    }
+}
