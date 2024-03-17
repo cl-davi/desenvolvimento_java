103254bc39608eb54e9e417ec98ae0cf97ce27a0
Davi <davifsclmt@gmail.com>
2024-03-16 21:02:19 -0300

    Exercício para verificar uma senha

---
+++ src/EstruturaRepetitiva/Exercicios/While/SenhaCorreta.java
@@ -0,0 +1,23 @@
+package EstruturaRepetitiva.Exercicios.While;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class SenhaCorreta {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String senha = "2002";
+
+        System.out.print("Digite a senha: ");
+        String senhaInserida = scanner.next();
+
+        while (!senhaInserida.equals(senha)) {
+            System.out.println("Senha Inválida");
+
+            System.out.print("Digite a senha corretamente: ");
+            senhaInserida = scanner.next();
+        }
+        System.out.println("Acesso permitido");
+    }
+}
