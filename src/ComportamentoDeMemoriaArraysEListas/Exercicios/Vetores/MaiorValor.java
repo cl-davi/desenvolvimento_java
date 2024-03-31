8cc2b25ab62ddec6dfad6729766f20c260ae12e6
Davi <davifsclmt@gmail.,com>
2024-03-31 05:47:53 -0300

    Exercício para exibir o maior valor no vetor

--- src/ComportamentoDeMemoriaArraysEListas/Exercicios/Vetores/MaiorValor.java
+++ src/ComportamentoDeMemoriaArraysEListas/Exercicios/Vetores/MaiorValor.java
@@ -1,9 +1,11 @@
 package ComportamentoDeMemoriaArraysEListas.Exercicios.Vetores;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class MaiorValor {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Quantos valores seraõ digitados: ");
