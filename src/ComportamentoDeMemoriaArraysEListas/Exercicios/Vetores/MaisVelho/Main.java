1537b53967ce813a0e9c0f0301397f7613354221
Davi <davifsclmt@gmail.,com>
2024-03-31 08:45:33 -0300

    Exercicio para exibir a pessoa mais velha de um array de objetos Pessoa

---
+++ src/ComportamentoDeMemoriaArraysEListas/Exercicios/Vetores/MaisVelho/Main.java
@@ -0,0 +1,35 @@
+package ComportamentoDeMemoriaArraysEListas.Exercicios.Vetores.MaisVelho;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Pessoa pessoa;
+
+        System.out.print("Quantas pessoas serão digitadas: ");
+        Pessoa[] vetor = new Pessoa[scanner.nextInt()];
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(STR."\nDigite os dados da \{i + 1}º pessoa: ");
+            System.out.print("Nome: ");
+            scanner.nextLine();
+            String nome = scanner.next();
+            System.out.print("Idade: ");
+            int idade = scanner.nextInt();
+
+            pessoa = new Pessoa(nome, idade);
+            vetor[i] = pessoa;
+        }
+
+        int maiorIdade = vetor[0].getIdade();
+        int posicaoMaior = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i].getIdade() > maiorIdade) {
+                maiorIdade = vetor[i].getIdade();
+                posicaoMaior = i;
+            }
+        }
+        System.out.println(STR."\nPESSOA MAIS VELHA: \{vetor[posicaoMaior].getNome()}");
+    }
+}
---
+++ src/ComportamentoDeMemoriaArraysEListas/Exercicios/Vetores/MaisVelho/Pessoa.java
@@ -0,0 +1,21 @@
+package ComportamentoDeMemoriaArraysEListas.Exercicios.Vetores.MaisVelho;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa() {}
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+}
