43c9811b90891909b73ae688605c9fb40ac2f35d
Davi <davifsclmt@gmail.,com>
2024-03-31 05:58:56 -0300

    Exercício para somar os vetores

--- src/ComportamentoDeMemoriaArraysEListas/Exercicios/Vetores/SomaVetores.java
+++ src/ComportamentoDeMemoriaArraysEListas/Exercicios/Vetores/SomaVetores.java
@@ -1,2 +1,30 @@
-package ComportamentoDeMemoriaArraysEListas.Exercicios.Vetores;public class SomaVetores {
+package ComportamentoDeMemoriaArraysEListas.Exercicios.Vetores;
+
+import java.util.Scanner;
+
+public class SomaVetores {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Quantos valores terão cada vetor: ");
+        int[] vetorA = new int[scanner.nextInt()];
+        int[] vetorB = new int[vetorA.length];
+        int[] vetorC = new int[vetorB.length];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.print(STR."Digite o \{i + 1}º valor do vetor A: ");
+            vetorA[i] = scanner.nextInt();
+        }
+        System.out.println();
+        for (int i = 0; i < vetorB.length; i++) {
+            System.out.print(STR."Digite o \{i + 1}º valor do vetor B: ");
+            vetorB[i] = scanner.nextInt();
+        }
+        System.out.println("\nVETOR RESULTANTE:");
+        for (int i = 0; i < vetorC.length; i++) {
+            vetorC[i] = vetorA[i] + vetorB[i];
+            System.out.println(vetorC[i]);
+        }
+        scanner.close();
+    }
 }
